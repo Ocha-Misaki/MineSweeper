244f4659a4a14ff3e257c3d08243d3cf2b8cf1ab
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-09-12 11:31:03 +0900

    爆弾の位置をランダムに指定

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 'use strict'
 {
-  for (let i = 0; i < 3; i++) {
+  const ceils = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]
+
+  let rowNum = 3
+  let colNum = 3
+
+  const makeBombPosition = (rowNum, colNum) => {
+    const bombRowNum = Math.floor(Math.random() * rowNum)
+    const bombColNum = Math.floor(Math.random() * colNum)
+    ceils[bombRowNum][bombColNum] = 1
+  }
+  makeBombPosition(rowNum, colNum)
+
+  for (let row = 0; row < rowNum; row++) {
     const tr = document.createElement('tr')
-    for (let col = 0; col < 3; col++) {
+    for (let col = 0; col < colNum; col++) {
       const td = document.createElement('td')
-      td.textContent = 1
+
+      td.textContent = ceils[row][col]
       tr.appendChild(td)
     }
     document.querySelector('div').appendChild(tr)
