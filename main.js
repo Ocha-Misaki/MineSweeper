cdf762b2642e59a7aedc67f330314a3f2b9fe5f8
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-09-14 11:59:40 +0900

    クラスにボムの情報を持たせるように変更

--- main.js
+++ main.js
@@ -4,15 +4,13 @@
   let colNum = 3
 
   const div = document.querySelector('div')
-  //スタートボタンを連続で押した時に、セルが増えないようにした
   const clearCeil = () => {
     while (div.firstChild) {
       div.removeChild(div.firstChild)
     }
   }
 
-  const setBombPosition = (rowNum, colNum) => {
-    //ceilsを関数内で定義することで、スタートを押すたびにリセットさせるようにした
+  const makeCeils = (rowNum, colNum) => {
     const ceils = []
     for (let row = 0; row < rowNum; row++) {
       ceils[row] = []
@@ -20,23 +18,17 @@
         ceils[row].push(new Ceil(row, col))
       }
     }
-    const bombRowNum = Math.floor(Math.random() * rowNum)
-    const bombColNum = Math.floor(Math.random() * colNum)
-    // ceils[bombRowNum][bombColNum] = '*'
-    makeCeil(ceils)
+    renderCeils(ceils)
     console.log(ceils)
   }
 
-  const makeCeil = (ceils) => {
+  const renderCeils = (ceils) => {
     for (let row = 0; row < rowNum; row++) {
       const tr = document.createElement('tr')
       for (let col = 0; col < colNum; col++) {
         const td = document.createElement('td')
         td.textContent = `${ceils[row][col].x} , ${ceils[row][col].y}`
-
-        td.addEventListener('click', () => {
-          checkCeil(td)
-        })
+        td.addEventListener('click', () => {})
         tr.appendChild(td)
       }
       div.appendChild(tr)
@@ -48,29 +40,37 @@
   document.querySelector('body').appendChild(start)
   start.addEventListener('click', () => {
     clearCeil()
-    safeNum = 0
-    setBombPosition(rowNum, colNum)
+    // safeNum = 0
+    makeCeils(rowNum, colNum)
   })
 
-  let safeNum = 0
-  const checkCeil = (td) => {
-    if (td.textContent == '*') {
-      td.classList.add('out')
-      confirm('game over…')
-    } else {
-      td.classList.add('safe')
-      safeNum++
-    }
+  // let safeNum = 0
+  // const checkCeil = (td) => {
+  //   if (td.textContent == '*') {
+  //     td.classList.add('out')
+  //     confirm('game over…')
+  //   } else {
+  //     td.classList.add('safe')
+  //     safeNum++
+  //   }
 
-    if (safeNum == 8) {
-      confirm('game clear!')
-    }
-  }
+  //   if (safeNum == 8) {
+  //     confirm('game clear!')
+  //   }
+  // }
 
   class Ceil {
     constructor(x, y) {
       this.x = x
       this.y = y
+      this.bombFlag = false
+    }
+
+    //ボムかどうか
+    check() {
+      if (this.bombFlag === true) {
+        this.textContent = '*'
+      }
     }
   }
 }
